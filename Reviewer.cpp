f2cafe502de24b33ef3a9648b1add6d057e67709
Requetetin <martinamadog@gmail.com>
2023-02-26 12:49:40 -0600

    ADD - Prelab

--- Reviewer.cpp
+++ Reviewer.cpp
@@ -16,7 +16,7 @@ void Reviewer::balancedParentheses(string regex) {
 void Reviewer::validOperations(string regex) {
   for (int i=0; i<regex.size(); i++) {
     if (regex[i] == '|') {
-      if (!isalnum(regex[i-1]) || !isalnum(regex[i + 1])) {
+      if ((!isalnum(regex[i-1]) && regex[i-1] != ')' && regex[i-1] != '*') || !isalnum(regex[i + 1]) && regex[i+1] != '(') {
         cout << "ERROR! Invalid | operation at index: " << i << endl;
         exit(-1);
       }
